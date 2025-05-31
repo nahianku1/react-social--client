27dc94991a2eb39ffc6cc42f7ea0e21c3a3f76a7
nahian islam <nahianku1@gmail.com>
2025-05-31 11:52:35 +0600

    some design improvements

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -33,6 +33,7 @@ import {
 import { Card, CardContent } from "@/components/ui/card";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { getCallTimeDiffs } from "@/utils/getCallTimeDiffs";
+import { MessageCircle } from "lucide-react";
 
 type message = {
   id: string;
@@ -948,7 +949,19 @@ function RouteComponent() {
             {/* Top Bar */}
 
             <div className="flex justify-between items-center px-6 py-4 bg-white/10 backdrop-blur-md shadow-md">
-              <h1 className="text-xl ml-8 font-semibold ">Chat App</h1>
+              <div className="text-xs sm:text-xl ml-7 sm:ml-0 font-semibold flex gap-1.5 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  width={24}
+                  height={24}
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path d="M256.6 8C116.5 8 8 110.3 8 248.6c0 72.3 29.7 134.8 78.1 177.9 8.4 7.5 6.6 11.9 8.1 58.2A19.9 19.9 0 0 0 122 502.3c52.9-23.3 53.6-25.1 62.6-22.7C337.9 521.8 504 423.7 504 248.6 504 110.3 396.6 8 256.6 8zm149.2 185.1l-73 115.6a37.4 37.4 0 0 1 -53.9 9.9l-58.1-43.5a15 15 0 0 0 -18 0l-78.4 59.4c-10.5 7.9-24.2-4.6-17.1-15.7l73-115.6a37.4 37.4 0 0 1 53.9-9.9l58.1 43.5a15 15 0 0 0 18 0l78.4-59.4c10.4-8 24.1 4.5 17.1 15.6z" />
+                </svg>
+                <span className="text-xs sm:text-xl">Messenger</span>
+              </div>
               <div className="flex gap-4 items-center justify-center">
                 {" "}
                 <DropdownMenu>
@@ -1004,8 +1017,8 @@ function RouteComponent() {
               </div>
             </div>
 
+            {/* Sidebar - Online Users (Desktop) */}
             <div className="flex flex-1 overflow-hidden">
-              {/* Sidebar - Online Users (Desktop) */}
               <div className="hidden sm:block w-64 p-3 space-y-2 overflow-y-auto h-full max-h-screen">
                 <h2 className="text-lg font-semibold mb-2">Online Users</h2>
                 <hr className="border-black" />
@@ -1054,7 +1067,7 @@ function RouteComponent() {
                   <SheetTrigger asChild>
                     <Button
                       size="icon"
-                      className="bg-white/30 text-black hover:bg-white/40"
+                      className="bg-white/30 text-black mt-1.5 hover:bg-white/40"
                     >
                       <span className="sr-only">Open Online Users</span>
                       <svg
